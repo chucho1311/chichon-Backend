49c8b61373875abea79813ff70f2b9fc375a1bcf
chucho1311 <carlosscristancho69@gmail.com>
2023-02-11 15:28:27 -0500

    clean data

--- src/users/usersRoutes.js
+++ src/users/usersRoutes.js
@@ -2,26 +2,13 @@ const router = require('express').Router();
 const passport = require('passport');
 const config = require('../config');
 const userServicesHandler = require('./usersServices');
-// require('../utils/auth')(passport);
 
 router 
     //? Rutas de usuarios
     .get("/users",userServicesHandler.getAllUsers)
     .get("/users/:id", userServicesHandler.getMyUserData)
-    // .get("/users/my-profile", passport.authenticate('jwt', config.jwtSecret), userServicesHandler.getMyUserData)
-    .post("/users", userServicesHandler.createUser)
+   .post("/users", userServicesHandler.createUser)
     .delete("/users/:userId", userServicesHandler.deleteUser)
-
-// router.route('/users')
-//     .get(
-//         userServicesHandler.getAllUsers
-//     );
-
-// router.route('/users/my-profile')
-//     .get(
-//         passport.authenticate('jwt', { session: false }),
-//         userServicesHandler.getMyUserData
-//     );
 
 module.exports = {
     router
